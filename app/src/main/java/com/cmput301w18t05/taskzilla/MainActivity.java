2a61ddf636426538a5593a65b658593602ec7d06
Wyatt <wyattpraharenka@gmail.com>
2018-02-22 10:04:05 -0700

    init

---
+++ app/src/main/java/com/cmput301w18t05/taskzilla/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cmput301w18t05.taskzilla;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
