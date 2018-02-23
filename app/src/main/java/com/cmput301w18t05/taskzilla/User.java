1a978e8c0f64385ea8e341bbc420050a24e166af
Wyatt <wyattpraharenka@gmail.com>
2018-02-23 14:14:54 -0700

    class stubs; Unit test for Task class

--- app/src/main/java/com/cmput301w18t05/taskzilla/User.java
+++ app/src/main/java/com/cmput301w18t05/taskzilla/User.java
@@ -10,7 +10,7 @@ public class User {
 
     private String name;
     private String username;
-    private Integer id;
+    private Integer ID;
     private PhoneNumber phone;
     private EmailAddress email;
     private Float providerRating;
@@ -23,4 +23,75 @@ public class User {
 
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getID() {
+        return ID;
+    }
+
+    public void setID(Integer ID) {
+        this.ID = ID;
+    }
+
+    public PhoneNumber getPhone() {
+        return phone;
+    }
+
+    public void setPhone(PhoneNumber phone) {
+        this.phone = phone;
+    }
+
+    public EmailAddress getEmail() {
+        return email;
+    }
+
+    public void setEmail(EmailAddress email) {
+        this.email = email;
+    }
+
+    public Float getProviderRating() {
+        return providerRating;
+    }
+
+    public void setProviderRating(Float providerRating) {
+        this.providerRating = providerRating;
+    }
+
+    public Float getRequesterRating() {
+        return requesterRating;
+    }
+
+    public void setRequesterRating(Float requesterRating) {
+        this.requesterRating = requesterRating;
+    }
+
+    public Integer getNumRequests() {
+        return numRequests;
+    }
+
+    public void setNumRequests(Integer numRequests) {
+        this.numRequests = numRequests;
+    }
+
+    public Integer getNumCompleteTasks() {
+        return numCompleteTasks;
+    }
+
+    public void setNumCompleteTasks(Integer numCompleteTasks) {
+        this.numCompleteTasks = numCompleteTasks;
+    }
 }
