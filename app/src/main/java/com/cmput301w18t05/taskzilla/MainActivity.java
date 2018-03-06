46d131b2d97599ae5a31f63ab56f2a39a8e325f9
Wyatt <praharen@ualberta.ca>
2018-03-05 20:38:38 -0700

    welcome screen and login

--- app/src/main/java/com/cmput301w18t05/taskzilla/MainActivity.java
+++ app/src/main/java/com/cmput301w18t05/taskzilla/MainActivity.java
@@ -1,7 +1,12 @@
 package com.cmput301w18t05.taskzilla;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * main activity includes the login screen
@@ -9,9 +14,27 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView usernameView;
+    private Button loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* setup view vars */
+        loginButton = findViewById(R.id.logInButton);
+
+        /* login action */
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /* TODO: implement username checking */
+
+                Intent loginIntent = new Intent(view.getContext(), WelcomeActivity.class);
+                startActivity(loginIntent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/cmput301w18t05/taskzilla/WelcomeActivity.java
+++ app/src/main/java/com/cmput301w18t05/taskzilla/WelcomeActivity.java
@@ -2,12 +2,43 @@ package com.cmput301w18t05.taskzilla;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TabHost;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    private TabHost tabs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        /* setup view */
+        tabs = findViewById(R.id.nav_tabs);
+
+        /* setup tabs */
+        tabs.setup();
+
+        TabHost.TabSpec profileTab = tabs.newTabSpec("Profile");
+        profileTab.setContent(R.id.Profile);
+        profileTab.setIndicator("Profile");
+        tabs.addTab(profileTab);
+
+        TabHost.TabSpec tasksTab = tabs.newTabSpec("Tasks");
+        profileTab.setContent(R.id.Tasks);
+        profileTab.setIndicator("Tasks");
+        tabs.addTab(profileTab);
+
+        TabHost.TabSpec mybidsTab = tabs.newTabSpec("My Bids");
+        profileTab.setContent(R.id.MyBids);
+        profileTab.setIndicator("My Bids");
+        tabs.addTab(profileTab);
+
+        TabHost.TabSpec searchTab = tabs.newTabSpec("Search");
+        profileTab.setContent(R.id.Search);
+        profileTab.setIndicator("Search");
+        tabs.addTab(profileTab);
+
     }
 }
