54b2a9c57efa60845e435a7c4c602a1c134ca79d
Wyatt <wyattpraharenka@gmail.com>
2018-03-05 19:21:25 -0700

    start on activities

--- app/src/main/java/com/cmput301w18t05/taskzilla/WelcomeActivity.java
+++ app/src/main/java/com/cmput301w18t05/taskzilla/WelcomeActivity.java
@@ -3,15 +3,11 @@ package com.cmput301w18t05.taskzilla;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-/**
- * main activity includes the login screen
- */
-
-public class MainActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_welcome);
     }
 }
