2600869c707483ecb4711d39b8e777a1747f2169
Jeremy <jng1@ualberta.ca>
2018-03-08 02:44:08 -0700

    added ElasticSearchController

---
+++ app/src/main/java/com/cmput301w18t05/taskzilla/ElasticSearchController.java
@@ -0,0 +1,25 @@
+package com.cmput301w18t05.taskzilla;
+
+import com.searchly.jestdroid.DroidClientConfig;
+import com.searchly.jestdroid.JestClientFactory;
+import com.searchly.jestdroid.JestDroidClient;
+
+/**
+ * Created by Jeremy
+ */
+
+public class ElasticSearchController {
+
+    private static JestDroidClient client;
+
+    private static void verifySettings() {
+        if (client == null) {
+            DroidClientConfig.Builder builder = new DroidClientConfig.Builder("http://cmput301.softwareprocess.es:8080/cmput301w18t05/");
+            DroidClientConfig config = builder.build();
+
+            JestClientFactory factory = new JestClientFactory();
+            factory.setDroidClientConfig(config);
+            client = (JestDroidClient) factory.getObject();
+        }
+    }
+}
