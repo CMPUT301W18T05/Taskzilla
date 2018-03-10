f659401babf2b294f453ad881b5e7fe96eda0cd5
Jeremy <jng1@ualberta.ca>
2018-03-09 21:04:06 -0700

    updated ESC

--- app/src/main/java/com/cmput301w18t05/taskzilla/ElasticSearchController.java
+++ app/src/main/java/com/cmput301w18t05/taskzilla/ElasticSearchController.java
@@ -12,6 +12,55 @@ public class ElasticSearchController {
 
     private static JestDroidClient client;
 
+    public void addTask(Task task) {
+
+    }
+
+    public void updateTask(Task task) {
+
+    }
+
+    public void removeTask(Task task) {
+
+    }
+
+    public Task searchTask(Task task) {
+        return task;
+    }
+
+    public void addUser(User user) {
+
+    }
+
+    public void updateUser(User user) {
+
+    }
+
+    public void removeUser(User user) {
+
+    }
+
+    public User searchUser(User user) {
+        return user;
+    }
+
+    public void addBid(Bid bid) {
+
+    }
+
+    public void updateBid(Bid bid) {
+
+    }
+
+    public void removeBid(Bid bid) {
+
+    }
+
+    public Bid searchBid(Bid bid) {
+        return bid;
+    }
+
+
     private static void verifySettings() {
         if (client == null) {
             DroidClientConfig.Builder builder = new DroidClientConfig.Builder("http://cmput301.softwareprocess.es:8080/cmput301w18t05/");
