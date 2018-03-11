1aaf2ab3b265025957c9820119d0469ff9c3a098
Micheal <mlnguyen@ualberta.ca>
2018-03-10 21:31:58 -0700

    Added Edit Button and activities it leads to

---
+++ app/src/main/java/com/cmput301w18t05/taskzilla/EditTaskActivity.java
@@ -0,0 +1,17 @@
+package com.cmput301w18t05.taskzilla;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by kio22 on 2018-03-10.
+ */
+
+public class EditTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_task);
+    }
+}
--- app/src/main/java/com/cmput301w18t05/taskzilla/ViewTaskActivity.java
+++ app/src/main/java/com/cmput301w18t05/taskzilla/ViewTaskActivity.java
@@ -3,17 +3,37 @@ package com.cmput301w18t05.taskzilla;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 
 public class ViewTaskActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_task);
+        Button EditButton = (Button) findViewById(R.id.EditButton);
+        int currentUserId = 5;                              //Dummy for Testing
+        int taskUserId = 5;                                 //Dummy for Testing
+        if(currentUserId == taskUserId){
+            EditButton.setVisibility(View.VISIBLE);
+        } else {
+            EditButton.setVisibility(View.INVISIBLE);
+        }
+
+
     }
 
     public void viewProfile(android.view.View view){
         Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
+    }
+
+    public void editTask(){
+        Intent intent = new Intent(this, EditTaskActivity.class);
         startActivity(intent);
     }
 }
