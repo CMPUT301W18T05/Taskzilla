50a44c9a9b6683ff3b419c077e093027512d8d41
Micheal <mlnguyen@ualberta.ca>
2018-03-11 23:56:32 -0600

    EditProfileActivity updates

--- app/src/main/java/com/cmput301w18t05/taskzilla/EditProfileActivity.java
+++ app/src/main/java/com/cmput301w18t05/taskzilla/EditProfileActivity.java
@@ -2,6 +2,7 @@ package com.cmput301w18t05.taskzilla;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class EditProfileActivity extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class EditProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+        User user = new User();
+        user.setName("Ken Wong");
+        user.setEmail(new EmailAddress("kennyw@ualberta.ca"));
+        user.setPhone(new PhoneNumber(7807553455L));
+        EditText Name = (EditText) findViewById(R.id.Name);
+        EditText Email = (EditText) findViewById(R.id.Email);
+        EditText Phone = (EditText) findViewById(R.id.Phone);
+        Name.setText(user.getName());
+        Email.setText(user.getEmail().toString());
+        Phone.setText(user.getPhone().toString());
+
+
     }
 }
