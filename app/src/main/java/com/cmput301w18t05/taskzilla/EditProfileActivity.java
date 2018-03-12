186015793babff722620bc4e71833fc315c7242b
Micheal <mlnguyen@ualberta.ca>
2018-03-12 00:15:45 -0600

    Email Error checking

--- app/src/main/java/com/cmput301w18t05/taskzilla/EditProfileActivity.java
+++ app/src/main/java/com/cmput301w18t05/taskzilla/EditProfileActivity.java
@@ -2,25 +2,51 @@ package com.cmput301w18t05.taskzilla;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 public class EditProfileActivity extends AppCompatActivity {
 
+
+    private User user = new User(); //dummy
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
-        User user = new User();
-        user.setName("Ken Wong");
-        user.setEmail(new EmailAddress("kennyw@ualberta.ca"));
-        user.setPhone(new PhoneNumber(7807553455L));
-        EditText Name = (EditText) findViewById(R.id.Name);
-        EditText Email = (EditText) findViewById(R.id.Email);
-        EditText Phone = (EditText) findViewById(R.id.Phone);
-        Name.setText(user.getName());
-        Email.setText(user.getEmail().toString());
-        Phone.setText(user.getPhone().toString());
+        EditText NameText = (EditText) findViewById(R.id.Name);
+        EditText EmailText = (EditText) findViewById(R.id.Email);
+        EditText PhoneText = (EditText) findViewById(R.id.Phone);
+        user.setName("Ken Wong"); //dummy
+        user.setEmail(new EmailAddress("kennyw@ualberta.ca")); //dummy
+        user.setPhone(new PhoneNumber(7807553455L)); //dummy
+        NameText.setText(user.getName());
+        EmailText.setText(user.getEmail().toString());
+        PhoneText.setText(user.getPhone().toString());
 
+    }
 
+    public void ProfileCancelButton(View view){
+        finish();
+    }
+
+    public void ProfileSaveButton(View view){
+        /** Add Save Code when ESC and controllers get figured out **/
+
+        /**                                         **/
+        EditText NameText = (EditText) findViewById(R.id.Name);
+        EditText EmailText = (EditText) findViewById(R.id.Email);
+        EditText PhoneText = (EditText) findViewById(R.id.Phone);
+        String email = EmailText.getText().toString();
+        int Errors = 0;
+        try{
+          user.setEmail(new EmailAddress(email));
+        } catch (IllegalArgumentException e){
+          EmailText.requestFocus();
+          EmailText.setError("Invalid Email");
+          Errors = 1;
+        }
+        if(Errors != 1) {
+            finish();
+        }
     }
 }
--- app/src/main/java/com/cmput301w18t05/taskzilla/EditTaskActivity.java
+++ app/src/main/java/com/cmput301w18t05/taskzilla/EditTaskActivity.java
@@ -7,25 +7,27 @@ import android.widget.EditText;
 
 public class EditTaskActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_task);
+        EditText TaskNameText = (EditText) findViewById(R.id.TaskName);
+        EditText DescriptionText = (EditText) findViewById(R.id.Description);
         Task task = new Task(); //Dummy Task
         task.setName("testTask"); //Dummy
         task.setDescription("testDescription"); //Dummy
-        EditText TaskName = (EditText) findViewById(R.id.TaskName);
-        EditText Description = (EditText) findViewById(R.id.Description);
-        TaskName.setText(task.getName());
-        Description.setText(task.getDescription());
+        TaskNameText.setText(task.getName());
+        DescriptionText.setText(task.getDescription());
 
     }
 
-    public void cancelButton(View view){
+    public void TaskCancelButton(View view){
         finish();
     }
 
-    public void saveButton(View view){
+    public void TaskSaveButton(View view){
         /** Add Save Code when ESC and controllers get figured out **/
 
         /**                                         **/
