b0ef0c625e8116e6d6a556d90a2e5fe844ace76b
Wyatt <wyattpraharenka@gmail.com>
2018-03-13 00:30:05 -0600

    RequestManager execute logic,connectivity change events implemented. todo: offline connectivity logic. also need to start creating request objects

--- app/src/main/java/com/cmput301w18t05/taskzilla/request/RequestManager.java
+++ app/src/main/java/com/cmput301w18t05/taskzilla/request/RequestManager.java
@@ -1,9 +1,5 @@
 package com.cmput301w18t05.taskzilla.request;
 
-/**
- * Created by wyatt on 07/03/18.
- */
-
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -13,42 +9,64 @@ import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import java.util.ArrayList;
 import java.util.Queue;
 import java.util.concurrent.TimeUnit;
 
 import static android.content.ContentValues.TAG;
 
 /**
- * RequestManager Singleton
+ * RequestManager SINGLETON
+ *
+ * Created by wyatt on 07/03/18.
  *
  * Abstraction for handling request objects
  * i.e backlog requests when app is not connected to
  * ElasticSearch server.
  *
+ * @author praharen wyatt
  */
 
 public class RequestManager extends BroadcastReceiver {
 
     private static final RequestManager instance = new RequestManager();
-    private Queue<Request> requestQueue;
     private boolean isConnected = false;
+    private static ArrayList<Request> requestQueue = new ArrayList<Request>();
 
-    private RequestManager() {
-    }
+    /* no constructor needed */
 
+    /* singleton class */
     public static RequestManager getInstance() {
         return instance;
     }
 
+    /**
+     * activities should call this method when wanting to
+     * execute a new request. request manager will handle the
+     * logic of offline/online connectivity.
+     * @param request the request object with instructions on
+     *                how to execute the request.
+     */
     public void invokeRequest(Request request) {
         if (isConnected) {
             request.execute();
         }
         else {
+            // todo: implement execute offline here!!
             requestQueue.add(request);
         }
     }
 
+    /**
+     * onReceive,
+     *
+     * called when connectivity status changes.
+     * This method then either executes waiting jobs if
+     * now online or ...
+     *
+     * @param context context when connectivity changes
+     * @param intent broadcast intent sent by android sys.
+     */
     @Override
     public void onReceive(final Context context, final Intent intent) {
         System.out.println("Network status change:");
@@ -60,20 +78,39 @@ public class RequestManager extends BroadcastReceiver {
             System.out.println("No ConnectivityManager in context.");
             this.isConnected = false;
         }
-        else if (ni.isConnected()) {
-            System.out.println("Network interface is not connected.");
-            this.isConnected = true;
-        }
         else if (!ni.isConnected()) {
-            System.out.println("Network is active.");
+            System.out.println("Network interface is not connected.");
             this.isConnected = false;
         }
+        else if (ni.isConnected()) {
+            System.out.println("Network is active.");
+            this.isConnected = true;
+            if (!requestQueue.isEmpty()) {
+                executeLogTask flushJobs = new executeLogTask();
+            }
+        }
     }
 
-    public void executeBacklog() {
-        /* needs to be async process */
+    /**
+     * executeLogTask is a wrapper to execute jobs in the request queue
+     * asynchronously.
+     */
+    public static class executeLogTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... voids) {
+            while (!requestQueue.isEmpty()) {
+                Request curRequest = popJob();
+                curRequest.execute();
+            }
+            return null;
+        }
     }
 
+    private static Request popJob() {
+        Request req = requestQueue.get(requestQueue.size()-1);
+        requestQueue.remove(requestQueue.size()-1);
+        return req;
+    }
         /*final PendingResult pendingResult = goAsync();
         AsyncTask<String, Integer, String> asyncTask = new AsyncTask<String, Integer, String>() {
             @Override
