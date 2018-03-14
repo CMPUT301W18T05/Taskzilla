864cc7878c9f906c3632aa3552c5e03c69aab29c
Colin Choi <cechoi@ualberta.ca>
2018-03-13 22:17:55 -0600

    New Task Activity and Controller Working

--- app/src/main/java/com/cmput301w18t05/taskzilla/NewTaskActivity.java
+++ app/src/main/java/com/cmput301w18t05/taskzilla/NewTaskActivity.java
@@ -2,14 +2,41 @@ package com.cmput301w18t05.taskzilla;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class NewTaskActivity extends AppCompatActivity {
 
+    private NewTaskController newTaskController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTitle("New Task");
+        setTitle("Add a Task");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_task);
+
+        newTaskController =  new NewTaskController(this);
+        Button cancel = (Button) findViewById(R.id.CancelButton);
+        Button addTask = (Button) findViewById(R.id.addTaskButton);
+        final EditText taskName = (EditText) findViewById(R.id.TaskName);
+        final EditText taskDescription = (EditText) findViewById(R.id.Description);
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                newTaskController.cancelTask();
+            }
+        });
+
+        addTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                newTaskController.addTask(taskName.getText().toString(),new User(), taskDescription.getText().toString());
+            }
+        });
+
     }
+
+
 }
