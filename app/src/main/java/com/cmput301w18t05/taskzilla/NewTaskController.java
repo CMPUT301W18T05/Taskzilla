36268b8ed2c5855a0e88af0044287c90b14361cf
Colin Choi <cechoi@ualberta.ca>
2018-03-16 14:20:40 -0600

    Charater Limit Constraints Added for NewTask

--- app/src/main/java/com/cmput301w18t05/taskzilla/NewTaskController.java
+++ app/src/main/java/com/cmput301w18t05/taskzilla/NewTaskController.java
@@ -1,6 +1,8 @@
 package com.cmput301w18t05.taskzilla;
 
 import android.content.Context;
+import android.text.TextUtils;
+import android.widget.EditText;
 
 import com.cmput301w18t05.taskzilla.request.RequestManager;
 import com.cmput301w18t05.taskzilla.request.command.AddTaskRequest;
@@ -21,14 +23,38 @@ public class NewTaskController {
 
     public void addTask(String name, User user, String description){
         //Check field lengths and give error
+        EditText taskName = (EditText) view.findViewById(R.id.TaskName);
+        EditText taskDescription = (EditText) view.findViewById(R.id.Description);
 
-        Task task = new Task(name,user,description);
-        //Add task to elastic search
+        if(TextUtils.getTrimmedLength(taskName.getText()) <= 25
+                && TextUtils.getTrimmedLength(taskName.getText()) >0
+                && TextUtils.getTrimmedLength(taskDescription.getText()) <= 25
+                && TextUtils.getTrimmedLength(taskDescription.getText()) >0){
+            Task task = new Task(name, user, description);
 
-        AddTaskRequest request = new AddTaskRequest(task);
-        RequestManager.getInstance().invokeRequest(ctx, request);
+            AddTaskRequest request = new AddTaskRequest(task);
+            RequestManager.getInstance().invokeRequest(ctx, request);
 
-        view.finish();
+            view.finish();
+        }else{
+
+            if (TextUtils.getTrimmedLength(taskName.getText()) > 25) {
+                taskName.setError("Name can not exceed 25 characters");
+            }
+
+            if (TextUtils.getTrimmedLength(taskName.getText()) == 0) {
+                taskName.setError("Name can not empty");
+            }
+
+            if (TextUtils.getTrimmedLength(taskDescription.getText()) > 280) {
+                taskDescription.setError("Description can not exceed 280 characters");
+            }
+            
+            if (TextUtils.getTrimmedLength(taskDescription.getText()) == 0) {
+                taskDescription.setError("Description can not be empty");
+            }
+
+        }
     }
 
     public void cancelTask(){
