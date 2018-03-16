2d0d3f43f376d1419ee248b1b00c8b9a9f7d06a9
Wyatt <wyattpraharenka@gmail.com>
2018-03-16 13:49:46 -0600

    search bids by user id

---
+++ app/src/main/java/com/cmput301w18t05/taskzilla/request/command/GetBidsByUserIdRequest.java
@@ -0,0 +1,45 @@
+package com.cmput301w18t05.taskzilla.request.command;
+
+import com.cmput301w18t05.taskzilla.Bid;
+import com.cmput301w18t05.taskzilla.ElasticSearchController;
+import com.cmput301w18t05.taskzilla.Task;
+import com.cmput301w18t05.taskzilla.request.Request;
+
+import java.util.ArrayList;
+
+/**
+ * Created by wyatt on 07/03/18.
+ */
+
+public class GetBidsByUserIdRequest extends Request {
+    ElasticSearchController.GetBidsByUserID task;
+    String userId;
+    ArrayList<Bid> result;
+
+    public GetBidsByUserIdRequest(String taskId) {
+        this.userId = taskId;
+    }
+
+    public void execute() {
+        task = new ElasticSearchController.GetBidsByUserID();
+        task.execute(this.userId);
+    }
+
+    @Override
+    public void executeOffline() {
+    }
+
+    public ArrayList<Bid> getResult() {
+        if (result != null) {
+            return result;
+        }
+
+        try {
+            result = this.task.get();
+            return result;
+        }
+        catch (Exception e) {
+            return null;
+        }
+    }
+}
