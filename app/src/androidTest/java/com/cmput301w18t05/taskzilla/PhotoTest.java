3954fd8fc90f646a73132eb6f6e8cd61c0bdce58
Jeremy <jng1@ualberta.ca>
2018-03-18 17:34:49 -0600

    Added copyright notice

--- app/src/androidTest/java/com/cmput301w18t05/taskzilla/PhotoTest.java
+++ app/src/androidTest/java/com/cmput301w18t05/taskzilla/PhotoTest.java
@@ -1,11 +1,12 @@
-/*PhotoTest
+/*
+ * Copyright 2018 (c) Andy Li, Colin Choi, James Sun, Jeremy Ng, Micheal Nguyen, Wyatt Praharenka
  *
- * Version 1.0
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
  *
- * Feb 23, 2018
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Copyright 2018 Team05, CMPUT301, University of Alberta - All Rights Reserved
- *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
  */
 package com.cmput301w18t05.taskzilla;
 
