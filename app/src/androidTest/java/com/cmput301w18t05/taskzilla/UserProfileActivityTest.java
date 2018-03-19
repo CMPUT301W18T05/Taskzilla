06f906eb6ee9653cf652a0da08009f718271a87f
Andy Li <ali5@ualberta.ca>
2018-03-19 02:25:05 -0600

    Profile intent testing

--- app/src/androidTest/java/com/cmput301w18t05/taskzilla/UserProfileActivityTest.java
+++ app/src/androidTest/java/com/cmput301w18t05/taskzilla/UserProfileActivityTest.java
@@ -11,30 +11,95 @@
 
 package com.cmput301w18t05.taskzilla;
 
+import android.app.ActionBar;
+import android.provider.ContactsContract;
+import android.support.design.widget.TabLayout;
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
+import com.cmput301w18t05.taskzilla.activity.EditProfileActivity;
 import com.cmput301w18t05.taskzilla.activity.MainActivity;
+import com.cmput301w18t05.taskzilla.activity.NewTaskActivity;
+import com.cmput301w18t05.taskzilla.activity.SignUpActivity;
+import com.cmput301w18t05.taskzilla.activity.WelcomeActivity;
 import com.cmput301w18t05.taskzilla.controller.ProfileController;
 import com.cmput301w18t05.taskzilla.fragment.ProfileFragment;
+import com.robotium.solo.Solo;
 
 /**
  * Created by Jeremy on 2018-02-23.
  */
 
 public class UserProfileActivityTest extends ActivityInstrumentationTestCase2 {
+    private Solo solo;
+
     public UserProfileActivityTest(){
         super(MainActivity.class);
     }
 
-
-    public void testProviderRatingUpdate() {
-        //ProfileFragment profileFragment = new ProfileFragment();
-        //User user = new User();
-        //ProfileController profileController = new ProfileController(user, profileFragment);
-        //profileController.updateProviderRating(10.0);
-        //assertEquals("10.0", profileFragment.getProviderRatingField().getText());
+    public void setUp() throws Exception{
+        solo = new Solo(getInstrumentation(), getActivity());
     }
 
+/*
+    public void testLogIn(){
+
+        //Set up for Test
+        MainActivity activity = (MainActivity)solo.getCurrentActivity();
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+        solo.clickOnText("Sign Up");
+        solo.assertCurrentActivity("Wrong Activity", SignUpActivity.class);
+        solo.enterText((EditText) solo.getView(R.id.usernameField), "TestUser");
+        solo.enterText((EditText) solo.getView(R.id.nameField), "TestName");
+        solo.enterText((EditText) solo.getView(R.id.emailField), "Test@Email.com");
+        solo.enterText((EditText) solo.getView(R.id.phoneField), "1234567890");
+        solo.clickOnButton("Sign Up");
+
+        //No Log in Info
+        solo.clickOnButton("Log In");
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+
+        //Incorrect Log in Info
+        solo.enterText((EditText) solo.getView(R.id.usernameText), "123456789101112131415161718192021222324252627282930");
+        solo.clickOnButton("Log In");
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+
+        //Correct Log in Info
+        solo.clearEditText((EditText) solo.getView(R.id.usernameText));
+        solo.enterText((EditText) solo.getView(R.id.usernameText), "TestUser");
+        solo.clickOnButton("Log In");
+        solo.assertCurrentActivity("Wrong Activity", WelcomeActivity.class);
+    }
+*/
+    public void testProfile() {
+
+        solo.enterText((EditText) solo.getView(R.id.usernameText), "TestUser");
+        solo.clickOnButton("Log In");
+        solo.assertCurrentActivity("Wrong Activity", WelcomeActivity.class);
+
+        solo.sleep(500);
+
+        solo.clickOnText("Profile");
+        solo.assertCurrentActivity("Wrong Activity", WelcomeActivity.class);
+
+        //Not implemented yet
+        //ImageButton editButton = (ImageButton) solo.getView(R.id.EditButton);
+        //solo.clickOnView(editButton);
+        //solo.sleep(500);
+        //solo.assertCurrentActivity("Wrong Activity", EditProfileActivity.class);
+
+        //checks if name of user matches name in profile
+        assertTrue(solo.waitForText(currentUser.getInstance().getName()));
+
+        //checks if email of user matches email in profile
+        assertTrue(solo.waitForText(currentUser.getInstance().getEmail().toString()));
+
+        //checks if phone of user matches phone in profile
+        assertTrue(solo.waitForText(currentUser.getInstance().getPhone().toString()));
+    }
 
 
 }
